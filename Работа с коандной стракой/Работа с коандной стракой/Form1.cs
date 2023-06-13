6e7e8faaef3681b6f8f507a441011c934242213c
DenisNSK <81846400+DenisNSK@users.noreply.github.com>
2023-06-13 09:32:39 +0700

    Add files via upload

---
+++ Работа с коандной стракой/Работа с коандной стракой/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Работа_с_коандной_стракой
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/C " + "shutdown /r");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/C " + "shutdown /s");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/C " + "shutdown /s/t 3600");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/C " + "shutdown /r/t 7200");
+        }
+    }
+}
